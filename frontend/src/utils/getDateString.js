428e8ae015525cf64155adc3e586f652938f4b7d
Nguyen Thanh Toan <nguyenthanhtoan2211@gmail.com>
2023-07-09 20:55:51 +0700

    fix date

--- frontend/src/utils/getDateString.js
+++ frontend/src/utils/getDateString.js
@@ -1,8 +1,8 @@
 export function getDateString(date) {
     if (typeof date === 'string')
         date = new Date(date)
-    const day = date.getDate() > 10 ? date.getDate() : `0${date.getDate()}`
-    const month = date.getMonth() + 1 > 10 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`
+    const day = date.getDate() >= 10 ? date.getDate() : `0${date.getDate()}`
+    const month = date.getMonth() + 1 >= 10 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`
     const year = date.getFullYear()
     return `${year}-${month}-${day}`
 }
